8b077c95358fd448c5c1b0296756260393ca2758
Andrei Varabyeu <andrei.varabyeu@gmail.com>
2015-02-20 19:20:35 +0300

    update git test

--- extensions/jashing-git/src/test/java/com/github/avarabyeu/jashing/eventsource/GitTest.java
+++ extensions/jashing-git/src/test/java/com/github/avarabyeu/jashing/eventsource/GitTest.java
@@ -1,30 +1,40 @@
 package com.github.avarabyeu.jashing.eventsource;
 
 import com.github.avarabyeu.jashing.integration.vcs.git.GitClient;
+import com.google.common.base.Joiner;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
+
 /**
  * @author Andrei Varabyeu
  */
 public class GitTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GitTest.class);
+
     @Test
     @Ignore
     public void testGit() {
         GitClient gitClient = new GitClient(
-                "git@github.com:avarabyeu/restendpoint.git", "restendpoint");
+                "git@github.com:avarabyeu/jashing.git", "jashing");
         LocalDate now = LocalDate.now();
-        Instant before = now.minusDays(4).atStartOfDay(ZoneId.systemDefault()).toInstant();
+        Instant before = now.minusMonths(12).atStartOfDay(ZoneId.systemDefault()).toInstant();
         Map<String, Integer> commitsPerUser = gitClient.
                 getCommitsPerUser(before.
                         atZone(ZoneId.systemDefault()).toInstant());
-        System.out.println(commitsPerUser);
+        LOGGER.info(Joiner.on(",").withKeyValueSeparator(":").join(commitsPerUser));
+        Assert.assertThat("GIT checkout doesn't work properly", commitsPerUser.entrySet(), not(empty()));
 
     }
 }
